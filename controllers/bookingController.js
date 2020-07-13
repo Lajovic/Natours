3e42f7fc80501fc4ba164ffad16ec1fb8d45fa8d
shakalel <igor.lajovic@gmail.com>
2020-07-13 14:13:08 -0300

    bugfix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -8,21 +8,22 @@ const Stripe = require('stripe')
 exports.getCheckoutSession = asyncCatcher(async (req, res, next) => {
 	const stripe = Stripe(process.env.STRIPE_KEY)
 	const tour = await Tour.findById(req.params.tourId)
+	const baseURL = `${req.protocol}://${req.get('host')}`
 	const session = await stripe.checkout.sessions.create({
 		payment_method_types: ['card'],
 		// URLs para testes de pagamento sem precisar que a aplicação esteja rodando hospedada (não usar em produção)
 		// success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${
 		// 	req.user.id
 		// }&price=${tour.price}`,
-		success_url: `${req.protocol}://${req.get('host')}/my-tours/`,
-		cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+		success_url: `${baseURL}/my-tours/`,
+		cancel_url: `${baseURL}/tour/${tour.slug}`,
 		customer_email: req.user.email,
 		client_reference_id: req.params.tourId,
-		line_items: [
+		display_items: [
 			{
 				name: `${tour.name} Tour`,
 				description: tour.summary,
-				images: [`https://natoursagency.herokuapp.com/img/tours/${tour.imageCover}`],
+				images: [`${baseURL}/img/tours/${tour.imageCover}`],
 				amount: tour.price * 100, // preço em centavos
 				currency: 'usd',
 				quantity: 1,
@@ -38,7 +39,8 @@ exports.getCheckoutSession = asyncCatcher(async (req, res, next) => {
 const createBookingCheckout = async(session) => {
 	const tour = session.client_reference_id
 	const user = await User.findOne({ email: session.customer_email })
-	const price = session.line_items[0].amount / 100 // de centavos pra dólares
+	const price = session.display_items[0].amount / 100 // de centavos pra dólares
+	console.log('tour, user, price: ', tour, user, price);
 	await Boooking.create(tour, user, price)
 }
 
@@ -52,7 +54,7 @@ exports.webhookCheckout = (req, res, next) => {
 		return res.status(400).send(`Webhook error: ${err.message}`)
 	}
 	
-	if (event.type === 'checkout.session.complete')
+	if (event.type === 'checkout.session.completed')
 		createBookingCheckout(event.data.object)
 	
 	res.status(200).json({ received: true })
