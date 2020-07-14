b8bc1ef178fa7dfca094e345fc0a251de41ec9cf
shakalel <igor.lajovic@gmail.com>
2020-07-14 04:31:34 -0300

    Fixing Stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -43,11 +43,12 @@ exports.getCheckoutSession = asyncCatcher(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
 	const tour = session.client_reference_id
 	const user = await User.findOne({ email: session.customer_email })
-	const price = session.line_items[0].amount / 100 // de centavos pra dólares
-	console.log(' --------- Tour: ', tour)
-	console.log(' --------- User: ', user)
-	console.log(' --------- Price: ', price)
-	await Boooking.create(tour, user, price)
+	const price = session.display_items[0].amount / 100 // de centavos pra dólares
+	try {
+		await Boooking.create(tour, user, price)
+	} catch (err) {
+		console.error('Houve um erro ao criar o booking da Tour.');
+	}
 }
 
 exports.webhookCheckout = asyncCatcher(async(req, res, next) => {
@@ -59,9 +60,8 @@ exports.webhookCheckout = asyncCatcher(async(req, res, next) => {
 	} catch (err) {
 		return res.status(400).send(`WebhookCheckout error: ${err.message}`)
 	}
-	if (event.type === 'checkout.session.completed')
-		console.log(event.data.object)
-		// await createBookingCheckout(event.data.object)
+	if (event.type === 'checkout.session.completed') 
+		await createBookingCheckout(event.data.object)
 
 	res.status(200).json({ received: true })
 })
